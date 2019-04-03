732ef1fabe492fd0f22a8b0c61131a3622a2a6e5
FrankSluyter <sluyterfrank@gmail.com>
2019-04-03 10:52:06 +1000

    all the game state event files added for menu, play, scoreboard and settings

--- Dodgeball/main.lua
+++ Dodgeball/main.lua
@@ -17,3 +17,22 @@ function game:link_event(event)
         end
     end
 end
+
+function game:change_state(state)
+    if self.states[state] ~= nil then
+        if self.states[self.current_state].exited ~= nil then
+            self.states[self.current_state].exited(self.states[self.current_state])
+        end
+
+        self.current_state = state
+
+        if self.states[self.current_state].entered ~= nil then
+            self.states[self.current_state].entered(self.states[self.current_state])
+        end
+    end
+end
+
+game:link_event("load")
+game:link_event("draw")
+game:link_event("update")
+game:link_event("keypressed")
---
+++ Dodgeball/play.lua
@@ -0,0 +1,3 @@
+local play = {}
+
+return play
