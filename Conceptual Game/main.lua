e2609d7bf86284780f850a85d6ee6b5f61b029f5
FrankSluyter <sluyterfrank@gmail.com>
2019-04-04 11:28:34 +1000

    made the main.lua more event based

--- Conceptual Game/main.lua
+++ Conceptual Game/main.lua
@@ -1,21 +1,15 @@
 game = {
     current_state = "menu",
     states = {
-        menu = {},
-        scoreboard = {},
-        editor = {},
-        playing = {
-            player_x = 0,
-            player_y = 0,
-            player_speed = 15,
-        },
+        menu = require("menu"),
+        play = require("play"),
+        scoreboard = require("scoreboard")
+        -- if we want settings in the future
+        -- settings = require("settings")
     }
 }
 
-function love.load()
-	assets = {
-    }
-end
+love.window.setTitle("Psycho Python")
 
 function game:link_event(event)
     love[event] = function(...)
@@ -27,32 +21,21 @@ function game:link_event(event)
     end
 end
 
+function game:change_state(state)
+    if self.states[state] ~= nil then
+        if self.states[self.current_state].exited ~= nil then
+            self.states[self.current_state].exited(self.states[self.current_state])
+        end
+
+        self.current_state = state
+
+        if self.states[self.current_state].entered ~= nil then
+            self.states[self.current_state].entered(self.states[self.current_state])
+        end
+    end
+end
+
+game:link_event("load")
 game:link_event("draw")
 game:link_event("update")
 game:link_event("keypressed")
-game:link_event("load")
-
-function game.states.menu:draw()
-    local gridXCount = 50
-    local gridYCount = 50
-    local cellSize = 10
-
-    love.graphics.setColor(.28, .28, .28)
-    love.graphics.rectangle(
-        'fill',
-        0,
-        0,
-        gridXCount * cellSize,
-        gridYCount * cellSize
-    )
-
-    love.graphics.setColor(0.0, 0.8, 0.4 )
-    love.graphics.print("Psycho Python\r\nThis is the menu state. Press space to start the game.", 90, 125
-)
-end
-
-function game.states.menu:keypressed(key)
-    if key == "space" then
-        game.current_state = "playing"
-    end
-end
