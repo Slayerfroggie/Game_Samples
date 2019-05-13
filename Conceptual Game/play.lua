c9978bf8d78cd4ea46dc4c34c4e9496120861671
Frank Sluyter <47513385+Slayerfroggie@users.noreply.github.com>
2019-05-13 14:54:10 +1000

    Started work on basic snake mechanics

--- Conceptual Game/play.lua
+++ Conceptual Game/play.lua
@@ -1,21 +1,17 @@
 local play = {
-    assests = {
+    assets = {
         score = love.graphics.newFont(20),
         default = love.graphics.getFont()
     },
-    pellets = {},
-    player = {
-        x = 0,
-        y = 0,
-        width = 0,
-        height = 0,
-        speed = 0
-    },
+    snake_segments = {},
+    foodPosition = {},
+    X_grid_count = 40,
+    Y_grid_count = 30,
     difficulty = 2,
-    sound = true,
-    pellet_score = 0
-    
+    timer = 0,
+    sound = true
 }
+
 function play:toggle_sound()
     self.sound = self.sound
     return self.sound
@@ -27,6 +23,57 @@ function play:toggle_difficulty()
         self.difficulty = 1
     end
     return self.difficulty
+end
+
+function play:entered()
+    self.snake_segments = {
+        {x = 3, y = 1},
+        {x = 2, y = 1},
+        {x = 1, y = 1},
+    }
+
+    self.timer = 0
+end
+
+function play:load()
+    
+
+end
+
+function play:draw()
+    local cell_size = 20
+
+    love.graphics.setColor(.28, .28, .28)
+    love.graphics.rectangle('fill', 0, 0, self.X_grid_count * cell_size, self.Y_grid_count * cell_size)
+
+    for segmentIndex, segment in ipairs(self.snake_segments) do
+        love.graphics.setColor(.6, 1, .32)
+        love.graphics.rectangle(
+            'fill',
+            (segment.x - 1) * cell_size,
+            (segment.y - 1) * cell_size,
+            cell_size - 1,
+            cell_size - 1
+        )
+    end
+end
+
+function love.update(dt)
+    self.timer = self.timer + dt
+    local timerLimit = 0.15
+
+    if self.timer >= timerLimit then
+        self.timer = self.timer - timerLimit
+
+        local nextXPosition = self.snake_segments[1].x + 1
+        local nextYPosition = self.snake_segments[1].y
+
+        table.insert(self.snake_segments, 1, {x = nextXPosition, y = nextYPosition})
+        table.remove(self.snake_segments)
+
+        love.graphics.setColor(232 / 255, 213 / 255, 185 / 255)
+        love.graphics.print("Tick" .. self.timer, 50, 50)
+    end
 end
 
 return play
